177ae10d48e0d61bb9e3fe317d5c9d6a9eb6d511
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2020-07-26 12:19:55 +0100

    sync function workers with sync workgourps

--- main.go
+++ main.go
@@ -10,43 +10,65 @@ import (
 )
 
 //TODO: add flags to the program
+//TODO: progress bar or status
+//TODO: support vcf format
+
 const filepath string = "./1.xlsx"
 
-var WG sync.WaitGroup
-
 func main() {
+	var WG sync.WaitGroup
 	var x xlsxhandler.XlsxData
 	var csF csvhandler.CsvFile
-	c := make(chan xlsxhandler.ChanResult)
-	cc := make(chan csvhandler.ChanRes)
-	n := xlsxhandler.New(filepath, "Sheet1", c)
-	x = n
-	err := x.Open()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("[*] Extracting Data \n")
+	var csvfilename string = "file.csv"
+
 	WG.Add(2)
-	go x.Dump()
-	D := <-c
+
+	c := make(chan xlsxhandler.ChanResult)
+	n := xlsxhandler.New(filepath, "Sheet2", c)
+	x = n
+
+	err := x.Open()
+	checkError(err)
+
+	excelData, err := workerI(&WG, &x, &c)
+	checkError(err)
+
+	workerII(&WG, csF, excelData, csvfilename)
+
+	WG.Wait()
+}
+
+func workerI(wg *sync.WaitGroup, x *xlsxhandler.XlsxData, c *chan xlsxhandler.ChanResult) ([][]string, error) {
+	fmt.Printf("[*] Extracting Data \n")
+	defer wg.Done()
+	xd := *x
+	go xd.Dump()
+	D := <-*c
 	if D.Err != nil {
-		log.Fatal(D.Err)
+		return [][]string{}, D.Err
 	}
-	WG.Done()
+	return D.CsvData, nil
+}
+
+func workerII(wg *sync.WaitGroup, csF csvhandler.CsvFile, DumpedData [][]string, csvfilename string) {
+	cc := make(chan csvhandler.ChanRes)
 	fmt.Printf("[*] Saving Data into csv file \n")
-	csvFD, err := csvhandler.New("file.csv", D.CsvData, cc)
-	if err != nil {
-		log.Fatal(err)
-	}
+	csvFD, err := csvhandler.New(csvfilename, DumpedData, cc)
+	checkError(err)
+	defer wg.Done()
 	csF = csvFD
 	go csF.WriteData()
 	V := <-cc
-	if V.Err != nil {
-		log.Fatal(V.Err)
-	}
+	checkError(V.Err)
 	if V.IsOk {
 		fmt.Printf("Finished\n")
-		WG.Done()
+	} else {
+		fmt.Printf("Failed\n")
 	}
-	WG.Wait()
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
